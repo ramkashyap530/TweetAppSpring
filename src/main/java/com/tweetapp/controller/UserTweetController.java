0f1bd81cf29bffe56c450ae244276ae4599b9f95
ramkashyap530 <51060430+ramkashyap530@users.noreply.github.com>
2022-06-01 23:48:10 +0530

    RAM 1

--- src/main/java/com/tweetapp/controller/UserTweetController.java
+++ src/main/java/com/tweetapp/controller/UserTweetController.java
@@ -25,7 +25,7 @@ public class UserTweetController {
 
     @GetMapping(path = "/test")
     public String gettest() {
-        return "it is done Jenkins 12";
+        return "it is done Jenkins 123";
     }
 
     @PostMapping(path = "/register")
